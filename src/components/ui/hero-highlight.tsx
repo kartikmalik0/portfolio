e1292dd8d67a79ab08c5f7791d556af210c0bae9
Kartik <kartik20044@gmail.com>
2024-06-19 07:03:33 -0300

    dark mode removed

--- src/components/ui/hero-highlight.tsx
+++ src/components/ui/hero-highlight.tsx
@@ -35,7 +35,7 @@ export const HeroHighlight = ({
       )}
       onMouseMove={handleMouseMove}
     >
-      <div className="absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800  pointer-events-none" />
+      <div className="absolute inset-0  dark:bg-dot-thick-neutral-800  pointer-events-none" />
       <motion.div
         className="pointer-events-none bg-dot-thick-indigo-500 dark:bg-dot-thick-[#0aff9d]   absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
         style={{
