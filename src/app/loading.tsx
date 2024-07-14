a005c22d389fba4ed6fb2e4a6fbd8793281b6c95
Kartik <kartik20044@gmail.com>
2024-07-14 14:31:13 -0300

    layout fixed

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,8 @@
-
-
 const Loading = () => {
     return (
-        <div className=" z-[1000] flex justify-center w-screen items-center h-screen">
+        <div className=" z-[1000] max-w-[100dwh] flex justify-center items-center h-screen">
             <div className="rounded-full mx-auto h-8 w-8 bg-lightgreen animate-ping"></div>
         </div>
     )
 }
-
 export default Loading;
--- src/providers/maxWidthWrapper.tsx
+++ src/providers/maxWidthWrapper.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react"
 
 const MaxWidthWrapper = ({children}:{children:ReactNode}) => {
   return (
-    <div className="w-16 mx-auto">
+    <div className="">
       {children}
     </div>
   )
