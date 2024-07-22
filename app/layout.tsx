76863f8f2db2756c43796c00646ea6667def072b
Kartik <kartik20044@gmail.com>
2024-07-22 04:32:25 -0300

    docs seo adde

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
+"use client"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import QueryProvider from "@/providers/queryProvider";
-import Head from "next/head"
+import { useRouter } from "next/router";
+import Head from "next/head";
 import 'fumadocs-ui/style.css';
-import Image from "next/image";
 import { RootProvider } from "fumadocs-ui/provider";
 const inter = Inter({ subsets: ["latin"] });
 
 const siteTitle = process.env.NEXT_PUBLIC_SITE_TITLE || "John Doe";
 const siteDescription = process.env.NEXT_PUBLIC_SITE_DESCRIPTION || "Software Engg.";
 const faviconUrl = process.env.NEXT_PUBLIC_FAVICON_URL || "/favicon.ico";
-const imageUrl = process.env.NEXT_PUBLIC_THUMBNAIL_IMAGE
+const imageUrl = process.env.NEXT_PUBLIC_THUMBNAIL_IMAGE;
 
+const docsTitle = "KartikDevHub";
+const docsDescription = "Explore to KARTIK_MALIKs Web Dev Documentation";
+const docsImageUrl = "https://utfs.io/f/724e983c-2b1a-4a1f-982b-2d8eaadd206e-gsff2r.jpg";
 
 export const metadata: Metadata = {
   title: siteTitle,
@@ -24,14 +28,22 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
+
+  const isDocsRoute = router.pathname.startsWith('/docs');
+
+  const currentTitle = isDocsRoute ? docsTitle : siteTitle;
+  const currentDescription = isDocsRoute ? docsDescription : siteDescription;
+  const currentImageUrl = isDocsRoute ? docsImageUrl : imageUrl;
+
   return (
     <html lang="en">
-      <head>
-        <title>{siteTitle}</title>
-        <meta name="description" content={siteDescription} />
+      <Head>
+        <title>{currentTitle}</title>
+        <meta name="description" content={currentDescription} />
         <link rel="icon" href={faviconUrl} />
-        {imageUrl && <meta property="og:image" content={imageUrl} />}
-      </head>
+        {currentImageUrl && <meta property="og:image" content={currentImageUrl} />}
+      </Head>
       <body className={inter.className}>
         <RootProvider>
           <QueryProvider>
