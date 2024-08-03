b5021e67f2a3b827e553c495f6e18b25adb2955e
Kartik <kartik20044@gmail.com>
2024-08-03 14:15:51 -0300

    skill suspense changes

--- src/components/suspenses/SkillsSuspense.tsx
+++ src/components/suspenses/SkillsSuspense.tsx
@@ -1,14 +1,13 @@
 import { Skeleton } from "../ui/suspense";
 
+
+const getRandomWidth = () => {
+    const minWidth = 3;
+    const maxWidth = 6;
+    return Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
+};
 const SkillsSuspense = ({length}:{length:number}) => {
     const array = new Array(length).fill(undefined);
-
-    const getRandomWidth = () => {
-        const minWidth = 3;
-        const maxWidth = 6;
-        return Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
-    };
-
     return (
         <>
             <ul className="flex w-full flex-wrap overflow-hidden gap-4 mb-6">
