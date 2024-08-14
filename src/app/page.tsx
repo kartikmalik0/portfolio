6520e50bc0ce6234fd0ea569000199869714dda0
Kartik <kartik20044@gmail.com>
2024-08-14 02:36:37 -0300

    testing about fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,37 @@ import Sidenav from "@/components/Sidenav";
 import { prisma } from "@/lib/connect";
 
 
+async function fetchHero() {
+  try {
+    console.log("fetching hero")
+    const hero = await prisma.hero.findMany();
+    return { hero };
+  } catch (error) {
+    throw new Error("Unable to fetch Hero");
+  }
+}
+
+export async function fetchAbout() {
+  console.log("fetching aobut")
+  try {
+      const about = await prisma.about.findMany();
+      return { about };
+  } catch (error) {
+      throw new Error("Unable to fetch About");
+  }
+}
 
 export default async function Home() {
-  async function fetchHero() {
-    try {
-      const hero = await prisma.hero.findMany();
-      return { hero };
-    } catch (error) {
-      throw new Error("Unable to fetch Hero");
-    }
-  }
 
   const data = await fetchHero();
+  const aboutData = await fetchAbout()
+
+  
 
   return (
     <div className="bg-[#080808] grid grid-cols-[60px_1fr]">
       <Sidenav />
-      <HomePage heroData={data.hero} />
+      <HomePage heroData={data.hero} aboutData = {aboutData.about}/>
     </div>
   );
 }
