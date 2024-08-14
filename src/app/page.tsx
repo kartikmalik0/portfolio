847d1c5ebd33ea64347b0ad72b4111a4b18131fb
Kartik <kartik20044@gmail.com>
2024-08-14 03:54:28 -0300

    prisma acclerate added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,33 @@
 import HomePage from "@/components/Home";
 import Sidenav from "@/components/Sidenav";
-import { prisma } from "@/lib/connect";
+import prisma from "@/lib/connect";
 
 
 async function fetchHero() {
   try {
-    const hero = await prisma.hero.findMany();
+    const hero = await prisma.hero.findMany({
+      cacheStrategy: {
+        ttl: 3600, // 1 hour
+        swr: 3600  // 1 hour
+      }
+    });
     return { hero };
   } catch (error) {
     throw new Error("Unable to fetch Hero");
   }
 }
 
- async function fetchAbout() {
+async function fetchAbout() {
   try {
-      const about = await prisma.about.findMany();
-      return { about };
+    const about = await prisma.about.findMany({
+      cacheStrategy: {
+        ttl: 3600, // 1 hour
+        swr: 3600  // 1 hour
+      }
+    });
+    return { about };
   } catch (error) {
-      throw new Error("Unable to fetch About");
+    throw new Error("Unable to fetch About");
   }
 }
 
@@ -26,12 +36,12 @@ export default async function Home() {
   const data = await fetchHero();
   const aboutData = await fetchAbout()
 
-  
+
 
   return (
     <div className="bg-[#080808] grid grid-cols-[60px_1fr]">
       <Sidenav />
-      <HomePage heroData={data.hero} aboutData = {aboutData.about}/>
+      <HomePage heroData={data.hero} aboutData={aboutData.about} />
     </div>
   );
 }
